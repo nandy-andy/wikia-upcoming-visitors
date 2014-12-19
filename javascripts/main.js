f8c0d62067c45a96edf0aa8a629eefc3cdc142d2
nAndy <nandy@wikia-inc.com>
2014-12-19 13:07:31 +0100

    removed console.logs and moved creation of <p>

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,12 +7,6 @@
 		future = new Date(today.getTime() + days*24*60*60*1000),
 		resultsDiv = document.getElementById('results');
 
-	console.log('== resultsDiv ==');
-	console.log(resultsDiv);
-
-	console.log('== filteredEvents ==');
-	console.log(filteredEvents);
-
 	function getCalendarEvents() {
 		gapi.client.load('calendar', 'v3', function() {
 			var request = gapi.client.calendar.events.list({
@@ -33,54 +27,31 @@
 	}
 
 	function filterResults(results) {
-		console.log('== results ==');
-		console.log(results);
-
-		console.log('== filteredEvents ==');
-		console.log(filteredEvents);
-
 		results.forEach(function(event) {
 			var matches = event.summary.match('(.{1,}) in (.{1,})'),
 				newEventObj;
 
 			if( matches ) {
-				console.log('match!');
-				newEventObj = {
+				filteredEvents.push({
 					who: matches[1],
 					where: matches[2],
 					summary: event.summary,
 					start: event.start.date,
 					end: event.end.date
-				};
-
-				filteredEvents.push(newEventObj);
-
-				console.log('== newEventObj ==');
-				console.log(newEventObj);
-				console.log('== filteredEvents ==');
-				console.log(filteredEvents);
+				});
 			}
 		});
-
-		console.log('== filteredEvents ==');
-		console.log(filteredEvents);
 
 		hideLoader();
 		displayResults();
 	}
 
 	function displayResults() {
-		console.log('== filteredEvents ==');
-		console.log(filteredEvents);
-
 		if( filteredEvents.length = 0 ) {
 			showError('It seems no one is traveling in next 30 days.');
 		} else {
-			var p = document.createElement('p');
-
 			filteredEvents.forEach(function(event) {
-				console.log(event);
-
+				var p = document.createElement('p');
 				p.appendChild(document.createTextNode(event.who + ' is in ' + event.where + '(' + event.start + ' - ' + event.end + ')'));
 				resultsDiv.appendChild(p);
 			});
